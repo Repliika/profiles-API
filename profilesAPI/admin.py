f48248baa3eeb761d219bbfb7ce9c554fa31acd2
Repliika <liddergg@gmail.com>
2021-12-31 09:20:08 -0500

    fixed registered admin siter with user profiles

--- profilesAPI/admin.py
+++ profilesAPI/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.db import models
 
 # Register your models here.
+
+from profilesAPI import models
+
+# registering the model as admin site so we can access via admin interface
+# registered user profiles to admin page
+admin.site.register(models.UserProfile)
